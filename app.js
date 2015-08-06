1906bac63a4e3a146da35b18ee85f59a989532c0
Tim Zallin <tim.zallin@gmail.com>
2015-08-06 23:33:28 +0300

    Updated index page

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 var api = require('./tel_api.js'),
     handlers = require('./handlers.js'),
-    http = require('https');
+    http = require('http'),
+    fs = require('fs');
 
 api.getUpdates();
 
 handlers(api);
 
 var server = http.createServer(function (req, res) {
-  res.end();
+  var readable = fs.createReadStream('index.html');
+
+  readable.on('data', function (chunk){
+    res.write(chunk);
+  });
+
+  readable.on('end', function (){
+    res.end();
+  });
+
 });
 
 server.listen(3000, function (){
