be5401b699ebcb3e90beb94e56d1d05d7ee90f17
Tim Zallin <tim.zallin@gmail.com>
2015-08-06 18:07:17 +0300

    Heroku deployment files added

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 var api = require('./tel_api.js'),
-    handlers = require('./handlers.js');
+    handlers = require('./handlers.js'),
+    http = require('https');
 
 api.getUpdates();
 
 handlers(api);
+
+var server = http.createServer(function (req, res) {
+  res.end();
+});
+
+server.listen(3000, function (){
+  console.log('server is up');
+});
